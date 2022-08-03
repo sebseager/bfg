9b2bdf6023b4e6e76bed3a1879de0f6f0b7fc920
Seb Seager <seb.seager@gmail.com>
2022-08-02 22:05:02 -0400

    Wrote out rest of signatures

--- bfg.c
+++ bfg.c
@@ -2,26 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-// struct bfg_info {
-//   char magic[4];
-//   uint32_t width;
-//   uint32_t height;
-// };
-*/
-
 typedef struct png_data {
   png_structp png_ptr;
   png_infop info_ptr;
-  /*
-  // png_uint_32 width;
-  // png_uint_32 height;
-  // png_byte bit_depth;
-  // png_byte color_type;
-  // png_byte n_channels;
-  */
   png_bytep *row_ptrs;
 } * png_data_t;
+
+typedef enum { GRAY, GRAY_ALPHA, RGB, RGB_ALPHA } bfg_color_type_t;
+
+/*
+ * Representation in memory of a decoded BFG image.
+ */
+typedef struct bfg_data {
+  uint32_t width;
+  uint32_t height;
+  uint8_t bit_depth;
+  uint8_t n_channels;
+  bfg_color_type_t color_type;
+  uint8_t palette_len;
+  uint8_t *pixels;
+  uint8_t *palette;
+} * bfg_data_t;
 
 /*
  * Standardize the row_ptrs array in png_data_t to contain all available color
@@ -60,17 +61,17 @@ typedef struct png_data {
  * After standardize_png, color_type will be either 0, 2, 4, or 6 with 1, 3, 2,
  * and 4 channels respectively.
  */
-int standardize_png(png_data_t data) {
-  png_set_expand(data->png_ptr); // expand PLTE and tRNS tables as needed
-  png_read_update_info(data->png_ptr, data->info_ptr); // update info_ptr
+int libpng_standardize(png_data_t png) {
+  png_set_expand(png->png_ptr); // expand PLTE and tRNS tables as needed
+  png_read_update_info(png->png_ptr, png->info_ptr); // update info_ptr
   return 0;
 }
 
 /*
  * Reads png file at fpath into png_data_t using the libpng API.
- * Caller is responsible for freeing the struct.
+ * Caller is responsible for freeing the struct with libpng_free.
  */
-int read_png(char *fpath, png_data_t data) {
+int libpng_read(char *fpath, png_data_t png) {
   FILE *fp = fopen(fpath, "rb");
   if (!fp) {
     fprintf(stderr, "Could not open file %s\n", fpath);
@@ -86,45 +87,74 @@ int read_png(char *fpath, png_data_t data) {
     return 1;
   }
 
-  data->png_ptr =
+  png->png_ptr =
       png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  if (!data->png_ptr) {
+  if (!png->png_ptr) {
     fprintf(stderr, "png_create_read_struct failed\n");
     fclose(fp);
     return 1;
   }
 
-  data->info_ptr = png_create_info_struct(data->png_ptr);
-  if (!data->info_ptr) {
+  png->info_ptr = png_create_info_struct(png->png_ptr);
+  if (!png->info_ptr) {
     fprintf(stderr, "png_create_info_struct failed\n");
-    png_destroy_read_struct(&data->png_ptr, NULL, NULL);
+    png_destroy_read_struct(&png->png_ptr, NULL, NULL);
     fclose(fp);
     return 1;
   }
 
-  png_init_io(data->png_ptr, fp);
-  png_set_sig_bytes(data->png_ptr, 8); // we've already read these 8 bytes
-  png_read_info(data->png_ptr, data->info_ptr);
+  png_init_io(png->png_ptr, fp);
+  png_set_sig_bytes(png->png_ptr, 8); // we've already read these 8 bytes
+  png_read_info(png->png_ptr, png->info_ptr);
 
   // don't access info_ptr directly
-  png_uint_32 height = png_get_image_height(data->png_ptr, data->info_ptr);
-  data->row_ptrs = malloc(sizeof(png_bytep) * height);
+  png_uint_32 height = png_get_image_height(png->png_ptr, png->info_ptr);
+  png->row_ptrs = malloc(sizeof(png_bytep) * height);
   for (png_uint_32 i = 0; i < height; i++) {
-    data->row_ptrs[i] = malloc(png_get_rowbytes(data->png_ptr, data->info_ptr));
+    png->row_ptrs[i] = malloc(png_get_rowbytes(png->png_ptr, png->info_ptr));
   }
 
-  if (standardize_png(data)) {
+  if (libpng_standardize(png)) {
     fprintf(stderr, "standardize_png failed\n");
-    png_destroy_read_struct(&data->png_ptr, &data->info_ptr, NULL);
+    png_destroy_read_struct(&png->png_ptr, &png->info_ptr, NULL);
     fclose(fp);
     return 1;
   }
 
-  png_read_image(data->png_ptr, data->row_ptrs);
+  png_read_image(png->png_ptr, png->row_ptrs);
 
   fclose(fp);
   return 0;
 }
+
+/*
+ * Allocates and populates bfg data struct with data from libpng data struct.
+ * Returns NULL on failure.
+ */
+bfg_data_t libpng_decode(png_data_t png) { return NULL; }
+
+/*
+ * Allocates and populates libpng data struct with data from bfg data struct.
+ * Returns NULL on failure.
+ */
+png_data_t libpng_encode(bfg_data_t bfg) { return NULL; }
+
+/*
+ * Writes new png file at fpath from given libpng data struct.
+ * Returns 0 on success, 1 on failure.
+ */
+int libpng_write(char *fpath, png_data_t png) { return 0; }
+
+/*
+ * Frees everything allocated within the data struct by libpng.
+ */
+void libpng_free(png_data_t png) {
+  png_free_data(png->png_ptr, png->info_ptr, PNG_FREE_ALL, -1);
+}
+
+// TODO: BFG FUNCTIONS START HERE
+
+
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -132,16 +162,16 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  png_data_t data = malloc(sizeof(struct png_data));
-  if (read_png(argv[1], data)) {
+  png_data_t png = malloc(sizeof(struct png_data));
+  if (libpng_read(argv[1], png)) {
     return 1;
   }
 
-  png_uint_32 height = png_get_image_height(data->png_ptr, data->info_ptr);
-  png_uint_32 width = png_get_image_width(data->png_ptr, data->info_ptr);
-  png_byte bit_depth = png_get_bit_depth(data->png_ptr, data->info_ptr);
-  png_byte color_type = png_get_color_type(data->png_ptr, data->info_ptr);
-  png_byte n_channels = png_get_channels(data->png_ptr, data->info_ptr);
+  png_uint_32 height = png_get_image_height(png->png_ptr, png->info_ptr);
+  png_uint_32 width = png_get_image_width(png->png_ptr, png->info_ptr);
+  png_byte bit_depth = png_get_bit_depth(png->png_ptr, png->info_ptr);
+  png_byte color_type = png_get_color_type(png->png_ptr, png->info_ptr);
+  png_byte n_channels = png_get_channels(png->png_ptr, png->info_ptr);
   printf("%d %d %d %d %d\n", height, width, bit_depth, color_type, n_channels);
 
   return 0;
